6452fe8ce91062754adefba105779f311003f076
vishal <infovishalkumar01@gmail.com>
2024-07-28 00:14:30 +0530

    resource

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+import Chai from './chai'
+
+
+function App() {
+  return (
+    <h1>code with react with Rohit</h1>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/chai.jsx
@@ -0,0 +1,11 @@
+
+function Chai(){
+    return(
+        <>
+        <Chai/>
+        {/* <h2>chai aur react with vishal</h2> */}
+        </>
+    );
+}
+
+export default Chai 
